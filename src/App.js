6ea6a5f3379fa4077f49462272b43cd1cf584285
Vaishnavi J <vaishnavi.j@mphasis.com>
2023-03-16 13:22:17 +0530

    change

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import SignInSignUp from "./Components/SignInSignUp";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <SignInSignUp />
+      {/* <Home /> */}
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// const App = () => {
+//   localStorage.setItem('name', 'Vaishnavi')
+//   console.log(localStorage.getItem('name'))
+//  // localStorage.removeItem('name')
+
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// localStorage.setItem('name', 'Vaishnavi')
+// console.log(localStorage.getItem('name'))
+
+// import React from 'react';
+
+// const App = () => {
+//   sessionStorage.setItem('name', 'Vangala')
+//   console.log(sessionStorage.getItem('name'))
+//   // sessionStorage.removeItem('name')
+
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
